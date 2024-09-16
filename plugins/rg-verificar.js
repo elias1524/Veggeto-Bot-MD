9d613fba0d2024ab0566f97952c170d91001fa9f
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-09-16 19:33:38 -0400

    🔮𝘝𝘦𝘨𝘨𝘦𝘵𝘰𝘉𝘰𝘵-MD🔮

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -1,45 +1,75 @@
-import db from '../lib/database.js'
 import { createHash } from 'crypto'
-import fs from 'fs'
-import fetch from 'node-fetch'
 
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+
+// Define la variable canales con la URL correcta
+const canales = 'https://whatsapp.com/channel/0029VaYh3Zm4dTnQKQ3VLT0h'; // Reemplaza con la URL correcta
+
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`🍭 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg*`)
-  if (!Reg.test(text)) return m.reply(`🌹 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.666*`)
+
+  if (user.registered === true) throw `*『✦』Ya estás registrado, para volver a registrarte, usa el comando: #unreg*`
+  if (!Reg.test(text)) throw `*『✦』El comando ingresado es incorrecto, uselo de la siguiente manera:*\n\n#reg *Nombre.edad*\n\n\`\`\`Ejemplo:\`\`\`\n#reg *${name2}.18*`
+
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply('🚩 El nombre no puede estar vacío.')
-  if (!age) return m.reply('🚩 La edad no puede estar vacía.')
-  if (name.length >= 100) return m.reply('🚩 El nombre es demasiado largo.' )
+
+  if (!name) throw '*『✦』No puedes registrarte sin nombre, el nombre es obligatorio. Inténtelo de nuevo.*'
+  if (!age) throw '*『✦』No puedes registrarte sin la edad, la edad es opcional. Inténtelo de nuevo.*'
+  if (name.length >= 30) throw '*『✦』El nombre no debe tener más de 30 caracteres.*' 
+
   age = parseInt(age)
-  if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
-  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
+
+  if (age > 999) throw '*『😏』¡Viejo/a Sabroso/a!*'
+  if (age < 5) throw '*¿𝐃𝐨𝐧𝐝𝐞 𝐞𝐬𝐭𝐚𝐧 𝐭𝐮𝐬 𝐩𝐚𝐩á𝐬?*😂'
+
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   global.db.data.users[m.sender].money += 600
-  global.db.data.users[m.sender].cookies += 15
+  global.db.data.users[m.sender].estrellas += 10
   global.db.data.users[m.sender].exp += 245
   global.db.data.users[m.sender].joincount += 5
-  let sn = createHash('md5').update(m.sender).digest('hex')
-let regbot = `👤 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗢 👤\n`
-regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-regbot += `「💭」𝗡𝗼𝗺𝗯𝗿𝗲: ${name}\n`
-regbot += `「✨️」𝗘𝗱𝗮𝗱: ${age} años\n`
-regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-regbot += `「🎁」𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:\n`
-regbot += `• 15 Cookies 🍪\n`
-regbot += `• 5 MiniCoins 🪙\n`
-regbot += `• 245 Experiencia 💸\n`
-regbot += `• 12 Tokens 💰\n`
-regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-regbot += `${packname}`
-await m.react('📩')
-await conn.sendMini(m.chat, '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰', textbot, regbot, imagen1, imagen1, channel, m)
+
+  let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
+  m.react('📩') 
+
+  let regbot = `╭══• ೋ•✧๑♡๑✧•ೋ •══╮
+*¡𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙾 𝙴𝚇𝙸𝚃𝙾𝚂𝙾!*
+╰══• ೋ•✧๑♡๑✧•ೋ •══╯
+║_-~-__-~-__-~-__-~-__-~-__-~-__-~-__-~-__-~-__-~-__
+║
+║ ֪ ׂ⛓️ ̶ ׁ ֪ 𝐍𝐨𝐦𝐛𝐫𝐞: ${name}
+║ ֪ ׁ🌫️  𝇌 𝐄𝐝𝐚𝐝: ${age} *Años*
+║
+║ *𝙶𝚛𝚊𝚌𝚒𝚜 𝚙𝚘𝚛 𝚛𝚎𝚐𝚒𝚜𝚝𝚛𝚊𝚛𝚝𝚎* 
+║📝 *𝚄𝚝𝚒𝚕𝚒𝚣𝚊* *.menu* *𝚙𝚊𝚛𝚊* *𝚟𝚎𝚛* *𝚎𝚕* *𝚖𝚎𝚗ú* *𝚍𝚎* *𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜.*
+║
+║
+║ ✨ 𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:
+║• 15 Estrellas 🌟
+║• 5 BlackCoins 🪙
+║• 245 Experiencia 💸
+║• 12 Tokens 💰
+╚══✦「꧙꧙꧙꧙꧙꧙꧙꧙꧙꧙꧙꧙」`
+
+  conn.sendMessage(m.chat, {
+    text: regbot,
+    contextInfo: {
+      externalAdReply: {
+        title: '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰',
+        body: wm, 
+        thumbnailUrl: 'https://telegra.ph/file/0bb7e9e7c8cb4e820f1fe.jpg', 
+        sourceUrl: canales,
+        mediaType: 1,
+        showAdAttribution: true,
+        renderLargerThumbnail: true,
+      }
+    }
+  }, { quoted: fkontak })
 }
+
 handler.help = ['reg']
 handler.tags = ['rg']
 handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
