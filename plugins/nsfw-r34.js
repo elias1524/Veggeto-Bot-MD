05003d82f984750917ff25875b3ce4787601d908
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-09-16 18:54:54 -0400

    🔮ᴠᴇɢɢᴇᴛᴛᴏ-ʙᴏᴛ-ᴍᴅ🔮

--- plugins/nsfw-r34.js
+++ plugins/nsfw-r34.js
@@ -59,7 +59,7 @@ if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '🚩 *¡Estos comandos
           mediaType: 1, 
           showAdAttribution: true,
           title: packname,
-          body: '🌟 Ai Yaemori - MD',
+          body: '🔮𝙑𝙚𝙜𝙜𝙚𝙩𝙩𝙤-𝘽𝙤𝙩-𝙈𝘿🔮',
           previewType: 0, 
           thumbnail: icons,
           sourceUrl: redes 
