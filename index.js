37fc6530778ce196ebc5000775ccb23e66112d72
Eliasivan <162828351+elias1524@users.noreply.github.com>
2024-10-19 12:04:45 -0300

    NEW VERSIÓN 1.0.1

--- index.js
+++ index.js
@@ -1,80 +1,79 @@
-import { join, dirname } from 'path';
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { setupMaster, fork } from 'cluster';
-import { watchFile, unwatchFile } from 'fs';
+console.log('iniciando Veggeto-Bot-MD 🔮');
+import {join, dirname} from 'path';
+import {createRequire} from 'module';
+import {fileURLToPath} from 'url';
+import {setupMaster, fork} from 'cluster';
+import {watchFile, unwatchFile} from 'fs';
 import cfonts from 'cfonts';
-import { createInterface } from 'readline';
+import {createInterface} from 'readline';
 import yargs from 'yargs';
-import chalk from 'chalk';
-console.log('\n✰ Iniciando Veggeto ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
-const { name, description, author, version } = require(join(__dirname, './package.json'));
-const { say } = cfonts;
+const {name, author} = require(join(__dirname, './package.json'));
+const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Veggeto\nBot\nMD', {
-font: 'block',
-align: 'center',
-colors: ['white']
-});
-say(`Multi Device`, {
-font: 'chrome',
-align: 'center',
-colors: ['red']
-});
-say(`Developed By • Ivan`, {
-font: 'console',
-align: 'center',
-colors: ['yellow']
-});
-var isRunning = false;
+
+say('Veggeto\n Bot\n MD\n', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']});
+say(`Nueva Version 1.0.1`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']});
+
+let isRunning = false;
+/**
+* Start a js file
+* @param {String} file `path/to/file`
+*/
 function start(file) {
-if (isRunning) return;
-isRunning = true;
-let args = [join(__dirname, file), ...process.argv.slice(2)];
-say([process.argv[0], ...args].join(' '), {
-font: 'console',
-align: 'center',
-colors: ['green']
-});
-setupMaster({
-exec: args[0],
-args: args.slice(1),
-});
-let p = fork();
-p.on('message', data => {
-switch (data) {
-case 'reset':
-p.process.kill();
-isRunning = false;
-start.apply(this, arguments);
-break;
-case 'uptime':
-p.send(process.uptime());
-break;
+  if (isRunning) return;
+  isRunning = true;
+  const args = [join(__dirname, file), ...process.argv.slice(2)];
+
+  say('Propietario By:Ivan', {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']});
+
+  setupMaster({
+    exec: args[0],
+    args: args.slice(1)});
+  const p = fork();
+  p.on('message', (data) => {
+    switch (data) {
+      case 'reset':
+        p.process.kill();
+        isRunning = false;
+        start.apply(this, arguments);
+        break;
+      case 'uptime':
+        p.send(process.uptime());
+        break;
+    }
+  });
+  p.on('exit', (_, code) => {
+    isRunning = false;
+    console.error('❗ㅤfallo Ocurrio un error inesperado:', code);
+
+    p.process.kill();
+    isRunning = false;
+    start.apply(this, arguments);
+
+    if (process.env.pm_id) {
+      process.exit(1);
+    } else {
+      process.exit();
+    }
+  });
+  const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+  if (!opts['test']) {
+    if (!rl.listenerCount()) {
+      rl.on('line', (line) => {
+        p.emit('message', line.trim());
+      });
+    }
+  }
 }
-});
-p.on('exit', (_, code) => {
-isRunning = false;
-console.error('🚩 Error:\n', code);
-process.exit();
-if (code === 0) return;
-watchFile(args[0], () => {
-unwatchFile(args[0]);
-start(file);
-});
-});
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim());
-});
-}
-process.on('warning', (warning) => {
-if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🚩 Se excedió el límite de Listeners en:');
-console.warn(warning.stack);
-}
-});
 start('sixfin.js');
